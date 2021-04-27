808b3e64c87279de24bda583802532d7574aca77
JocelynePT <56885388+JocelynePT@users.noreply.github.com>
2021-04-27 11:56:09 +0100

    Aula de hoje com os mapas

--- app/controllers/dMap.js
+++ app/controllers/dMap.js
@@ -1,3 +1,30 @@
+var MapModule = require("ti.map");
+
+//Ti.Geolocation.accuracy = Ti.Geolocation.ACCURACY_HIGH;
+
+var mapView = MapModule.createView({
+    mapType : MapModule.NORMAL_TYPE,
+    animate : true,
+    userLocation : true,
+    zoom : 8
+});
+
+$.dMap.addEventListener('open', function() {
+    console.log(Ti.Geolocation.hasLocationPermissions());
+    if (Ti.Geolocation.hasLocationPermissions()) {
+        $.map_view.add(mapView);
+      } else {
+        Ti.Geolocation.requestLocationPermissions(Ti.Geolocation.AUTHORIZATION_ALWAYS, function(e) {
+            if (e.success) {
+                $.map_view.add(mapView);
+            } else {
+                alert('Could not obtain location permissions');
+                $.dMap.close();
+            }
+        });
+    }
+});
+
 function go_back () {
     var actionBar = $.dMap.activity.actionBar; 
     actionBar.onHomeIconItemSelected = function() { 
@@ -5,7 +32,9 @@ function go_back () {
     };
 }
 
-function nextView() {
+
+
+/* function nextView() {
     var dCategory = Alloy.createController("dCategory").getView();
     dCategory.open();
-}
+} */
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,9 @@
 $.win.open();
-
+/* 
 function nextView() {
     var dMain = Alloy.createController("dMain").getView();
     dMain.open();
-}
+} */
+var dMap = Alloy.createController("dMap").getView();
+    dMap.open();
+
